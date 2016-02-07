109f1f5a66c514b3f1a57bf80a19f53b50ea38bd
nosami <jasonimison@gmail.com>
2016-02-07 16:59:50 +0000

    init

---
+++ fsconsoleresource/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace fsconsoleresource
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("fsconsoleresource")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("jason")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ fsconsoleresource/Program.fs
@@ -0,0 +1,8 @@
+﻿open System
+open System.Reflection
+
+[<EntryPoint>]
+let main argv = 
+    Assembly.GetExecutingAssembly().GetManifestResourceNames()
+    |> Array.iter (Console.WriteLine) 
+    0 // return an integer exit code    
